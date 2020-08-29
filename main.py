ba6da7da252554d99b867d3a39b338453c2c9b55
leo2105 <leoleonvera20@hotmail.com>
2020-08-29 14:06:25 -0300

    read data

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 from classes.graph import GraphBFS
-moves_16 = [
+from utils.read_input import read_input
+
+try:
+    filepath = sys.argv[1]
+except:
+    filepath = 'samples/sample.txt'
+
+init_matrix, goal_matrix = read_input(filepath)
+
+"""
+init_matrix = [
     [2,3,'x'],
     [1,5,6],
     [4,7,8],
 ]
-goal = [
+goal_matrix = [
     [1,2,3],
     [4,5,6],
     [7,8,'x'],
 ]
-# goal_state = [
-#     ['x', 2, 3],
-#     [4, 5, 6],
-#     [7, 8, 1],
-# ]
-
-moves = GraphBFS().bfs(moves_16, goal)
+"""
+moves = GraphBFS().bfs(init_matrix, goal_matrix)
 print(moves)
---
+++ utils/read_input.py
@@ -0,0 +1,28 @@
+import copy
+
+def read_input(filepath):
+    
+    with open(filepath) as fp:
+        line = fp.readline()
+        N = int(line[0])
+        init_matrix = [[None for x in range(N)] for y in range(N)] 
+        goal_matrix = copy.deepcopy(init_matrix)
+        i = 0
+        while True:
+            line = fp.readline()
+            if not line:
+                break
+            line = line[:-1] if line[-1] == '\n' else line
+            numbers = line.split(',')
+            if numbers[0] == '-':
+                continue
+            assert len(numbers) == N            
+
+            for j in range(N):
+                goal_matrix[i][j] = i*N+j+1
+                init_matrix[i][j] = numbers[j] if numbers[j] == 'x' else int(numbers[j])
+            goal_matrix[N-1][N-1] = 'x'
+            i+=1
+            
+
+    return init_matrix, goal_matrix
